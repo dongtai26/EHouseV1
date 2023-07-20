938291120b84b93f1d4b3288fa00539a32370049
Lê Tân <letan01674@gmail.com>
2023-07-21 00:27:52 +0700

    add HouseRentName and RentPrice in table Contract

---
+++ BussinessObjects/Migrations/20230720172051_updateDB1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObjects.Migrations
+{
+    public partial class updateDB1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HouseRentName",
+                table: "Contracts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<float>(
+                name: "RentPrice",
+                table: "Contracts",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HouseRentName",
+                table: "Contracts");
+
+            migrationBuilder.DropColumn(
+                name: "RentPrice",
+                table: "Contracts");
+        }
+    }
+}
